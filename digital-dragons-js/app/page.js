05ee1856dd6ca6b2ef585690e0dcf6d98ff90788
DESKTOP-1NR5180\gero <42528358+GeroLeLan@users.noreply.github.com>
2023-05-26 17:06:31 -0300

    Vista de barra de navegacion y pagina principal

--- digital-dragons-js/app/page.js
+++ digital-dragons-js/app/page.js
@@ -1,7 +1,29 @@
-export default function Home() {
-  return (
-    <secction className="w-full flex-center flex-col">
-      <h1>Digital Travel</h1>
-    </secction>
-  )
-}
+
+  import React from 'react';
+
+  export default function Home() {
+    return (
+      <div className="bg-gray-100">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="py-8">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight text-center">
+              ¡Reserva tus vuelos fácilmente!
+            </h1>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto text-center">
+              Bienvenido a nuestra plataforma de reserva de vuelos. Encuentra las mejores opciones de vuelos y reserva de forma rápida y sencilla.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <a
+                href="#"
+                className="px-6 py-3 text-base font-medium text-white bg-blue-500 hover:bg-blue-600 rounded-lg"
+              >
+                Ver vuelos disponibles
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  
--- digital-dragons-js/components/Navbar.js
+++ digital-dragons-js/components/Navbar.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-blue-500">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <a href="#" className="text-white font-semibold text-lg">
+              Digital Travels
+            </a>
+          </div>
+          <div className="flex">
+            <a href="#" className="text-white hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium">
+              Inicio
+            </a>
+            <a href="#" className="text-white hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium">
+              Reservas
+            </a>
+            <a href="#" className="text-white hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium">
+              About Us
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
