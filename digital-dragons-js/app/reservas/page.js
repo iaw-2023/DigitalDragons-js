fe4a284c903488c86f503afaee62d30f8925630b
aldanacase <42502665+aldanacase@users.noreply.github.com>
2023-06-03 21:40:29 -0300

    Construyendo pagina de Reservas
    Se agregó la pagina principal para las reservas junto con el componente que se utiliza para mostrar las reservas. Se generaron ejemplos de prueba.

---
+++ digital-dragons-js/app/reservas/page.js
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react';
+import CardReservas from '../../components/CardReservas';
+
+
+
+const reservas = () => {
+    // Aquí puedes realizar cualquier lógica adicional, como obtener las reservas del usuario desde una API
+  
+    // Ejemplo de datos de reservas
+          
+  
+    return (
+      <div>
+      
+      
+        <CardReservas />
+      
+    </div>
+    );
+  };
+  
+  export default reservas;
+  
