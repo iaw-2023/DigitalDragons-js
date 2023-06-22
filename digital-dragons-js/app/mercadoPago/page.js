c360ec579feee4804c86cb537a1261aec2597908
DESKTOP-1NR5180\gero <42528358+GeroLeLan@users.noreply.github.com>
2023-06-22 14:55:21 -0300

    Prueba de Mercado Pago

---
+++ digital-dragons-js/app/mercadoPago/page.js
@@ -0,0 +1,89 @@
+"use client"
+import React, { useEffect } from 'react';
+
+export default function MP() {
+  useEffect(() => {
+    const loadMercadoPagoScript = () => {
+      const script = document.createElement('script');
+      script.src = 'https://sdk.mercadopago.com/js/v2';
+      script.async = true;
+      script.onload = initializeMercadoPago;
+      document.body.appendChild(script);
+    };
+
+    const initializeMercadoPago = () => {
+      const mp = new window.MercadoPago('TEST-4aab04bf-a4fb-4724-881f-53f0ffc5a971', {
+        locale: 'es'
+      });
+
+      const bricksBuilder = mp.bricks();
+      const renderCardPaymentBrick = async (bricksBuilder) => {
+        const settings = {
+          initialization: {
+            amount: 100, // monto a ser pago
+            payer: {
+              email: ''
+            }
+          },
+          customization: {
+            visual: {
+              style: {
+                theme: 'default' // | 'dark' | 'bootstrap' | 'flat'
+              }
+            }
+          },
+          callbacks: {
+            onReady: () => {
+              // callback llamado cuando Brick esté listo
+            },
+            onSubmit: (cardFormData) => {
+              //  callback llamado cuando el usuario haga clic en el botón enviar los datos
+              //  ejemplo de envío de los datos recolectados por el Brick a su servidor
+              return new Promise((resolve, reject) => {
+                fetch('/process_payment', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(cardFormData)
+                })
+                  .then((response) => {
+                    // recibir el resultado del pago
+                    resolve();
+                  })
+                  .catch((error) => {
+                    // tratar respuesta de error al intentar crear el pago
+                    reject();
+                  });
+              });
+            },
+            onError: (error) => {
+              // callback llamado para todos los casos de error de Brick
+            }
+          }
+        };
+
+        window.cardPaymentBrickController = bricksBuilder
+          .create('cardPayment', 'cardPaymentBrick_container', settings)
+          .catch((error) => {
+            console.error('Error creating card payment brick:', error);
+          });
+      };
+
+      renderCardPaymentBrick(bricksBuilder);
+    };
+
+    loadMercadoPagoScript();
+  }, []);
+
+  return (
+    <html>
+      <head>
+        <title>MercadoPago Example</title>
+      </head>
+      <body>
+        <div id="cardPaymentBrick_container"></div>
+      </body>
+    </html>
+  );
+}
