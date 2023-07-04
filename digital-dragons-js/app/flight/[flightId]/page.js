5f095ad65a7b778433479a0c17f18c9475366779
DESKTOP-1NR5180\gero <42528358+GeroLeLan@users.noreply.github.com>
2023-07-04 17:27:29 -0300

    ahora es resposive lo que antes era una tabla
    ademas anda el boton de reserva que se habia roto

--- digital-dragons-js/app/flight/[flightId]/page.js
+++ digital-dragons-js/app/flight/[flightId]/page.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { useRouter } from 'next/navigation';
+import FlightCategory from '../../../components/FlightCategory';
 
 const FlightPage = ({ params }) => {
   const router = useRouter();
@@ -114,15 +115,7 @@ const FlightPage = ({ params }) => {
       reservationData = JSON.stringify(reservationData);
       localStorage.setItem('reservationData', reservationData);
       
-      
-
-
-      // try {
-      //   // Realizar la solicitud POST para registrar la reserva
-      //   const response = await axios.post(
-      //     'https://digital-dragons-laravel-2rwz5slqh-digitaldragons.vercel.app/rest/reservas',
-      //     reservationData
-      //   );
+  
           Swal.fire({
             title: 'Datos de reserva',
             html: `
@@ -143,17 +136,13 @@ const FlightPage = ({ params }) => {
               // Lógica a realizar cuando se hace clic en el botón "Cancelar" o se cierra la alerta
             }
           });
-          
-      // } catch (error) {
-      //   console.error('Error al registrar la reserva:', error);
-      // }
     }else{
       alert("Lamentamos informarle que se acaba de ocupar el ultimo asiento disponible para este vuelo :(")
     }
   };
 
   const handleCategoryChange = (category) => {
-    let backgroundImageClass = imageUrl ? 'bg-cover bg-center h-full flex flex-col items-center justify-center with-background' : 'bg-cover bg-center h-full flex flex-col items-center justify-center';
+    //let backgroundImageClass = imageUrl ? 'bg-cover bg-center h-full flex flex-col items-center justify-center with-background' : 'bg-cover bg-center h-full flex flex-col items-center justify-center';
 
     setSelectedCategory(category);
   };
@@ -173,121 +162,66 @@ const FlightPage = ({ params }) => {
   return (
     <div className="page-container bg-gray-100 h-screen">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
-        <div id="midiv" >
         
-          
+        
+        <div
+              className={backgroundImageClass}
+              style={{ backgroundImage: `url(${imageUrl})`,
+              }}
+            >
         
         
 
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight text-center text-black border-3 p-4">
             ¡Ya estás a un paso de {flightData.destino}!
           </h1>
-          <p className="mt-4 max-w-2xl text-xl text-black lg:mx-auto text-center border-3 p-4">
+          <p className="mt-4 max-w-2xl bg-white bg-opacity-50 rounded text-xl text-black lg:mx-auto text-center border-3 p-4">
           
-          <img src={imageUrl} alt="Destino" style={{ width: '700px', height: 'auto' }} />
+          {/* <img src={imageUrl} alt="Destino" style={{ width: '700px', height: 'auto' }} /> */}
 
           
             Selecciona la categoría de vuelo perfecta y haz de tu viaje una experiencia inolvidable
           </p>
           
 
-
-          <section className="landing-section center text-black mt-4">
-            <h2 className="title__group"></h2>
-            <table className="average-price-flights w-full border-collapse">
-              <thead>
-                <tr>
-                  <th id="headerCategory" className="bg-gray-100 py-2 px-4">
-                    <span className="sr-only">Categoría de pasaje</span>
-                  </th>
-                  <th id="category_0" className="bg-gray-100 py-2 ">
-                    Económica
-                  </th>
-                  <th id="category_1" className="bg-gray-100 py-2 px-4">
-                    Premium Economy
-                  </th>
-                  <th id="category_2" className="bg-gray-100 py-2 px-4">
-                    Business
-                  </th>
-                  <th id="category_3" className="bg-gray-100 py-2 px-4">
-                    Primera clase
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr data-link="/t/barometro-tarifas/flight/economica" title="Ver el barómetro de tarifas de pasajes económicos">
-                  <th headers="headerCategory" id="category_0" className="py-4 px-6">
-                    Categorias Disponibles
-                  </th>
-                  <td headers="category_0 category_1" data-info="Economica" className="py-4 px-6">
-                  {flightData.precio}$
-                  </td>
-                  <td headers="category_0 category_2" data-info="Premium Economy" className="py-4 px-6">
-                    
-                  {Math.floor(flightData.precio * 1.3)}$
-                  </td>
-                  <td headers="category_0 category_3" data-info="Business" className="py-4 px-6">
-                  {Math.floor(flightData.precio * 1.7)}$
-                  </td>
-                  <td headers="category_0 category_3" data-info="Primera clase" className="py-4 px-6">
-                  {Math.floor(flightData.precio * 2.1)}$
-                  </td>
-                </tr>
-                <tr>
-                  <th></th>
-                  <td>
-                    <button
-                      className={`${
-                        selectedCategory === 'Economica' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-                      } ml-2 px-4 py-1 rounded`}
-                      onClick={() => handleCategoryChange('Economica')}
-                    >
-                      {selectedCategory === 'Economica' ? 'Seleccionado' : 'Seleccionar'}
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className={`${
-                        selectedCategory === 'Premium Economy' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-                      } ml-2 px-4 py-1 rounded`}
-                      onClick={() => handleCategoryChange('Premium Economy')}
-                    >
-                      {selectedCategory === 'Premium Economy' ? 'Seleccionado' : 'Seleccionar'}
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className={`${
-                        selectedCategory === 'Business' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-                      } ml-2 px-4 py-1 rounded`}
-                      onClick={() => handleCategoryChange('Business')}
-                    >
-                      {selectedCategory === 'Business' ? 'Seleccionado' : 'Seleccionar'}
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className={`${
-                        selectedCategory === 'Primera clase' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-                      } ml-2 px-4 py-1 rounded`}
-                      onClick={() => handleCategoryChange('Primera clase')}
-                    >
-                      {selectedCategory === 'Primera clase' ? 'Seleccionado' : 'Seleccionar'}
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </section>
+<section className="landing-section  center text-black mt-4">
+      <h2 className="title__group"></h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 bg-white bg-opacity-50 rounded gap-4">
+        <FlightCategory
+          title="Económica"
+          price={flightData.precio}
+          selectedCategory={selectedCategory}
+          handleCategoryChange={handleCategoryChange}
+        />
+        <FlightCategory
+          title="Premium Economy"
+          price={Math.floor(flightData.precio * 1.3)}
+          selectedCategory={selectedCategory}
+          handleCategoryChange={handleCategoryChange}
+        />
+        <FlightCategory
+          title="Business"
+          price={Math.floor(flightData.precio * 1.7)}
+          selectedCategory={selectedCategory}
+          handleCategoryChange={handleCategoryChange}
+        />
+        <FlightCategory
+          title="Primera clase"
+          price={Math.floor(flightData.precio * 2.1)}
+          selectedCategory={selectedCategory}
+          handleCategoryChange={handleCategoryChange}
+        />
+      </div>
+    </section>
           <div className="mt-10 flex justify-center text-black"></div>
           <form className="text-black" onSubmit={handleSubmit}>
 
 <button
   disabled={!selectedCategory || !localStorage.getItem('access_token')}
-
   id="btnRealizarReserva"
   type="submit"
-  className={`mt-4 ${!(!selectedCategory || !localStorage.getItem('access_token')) ? 'border-2 border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500' : 'border-2 border-black'} font-bold py-2 px-4 rounded`}
+  
+  className={`mt-4 ${(selectedCategory && localStorage.getItem('access_token')) ? 'border-2 border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500' : 'border-2 border-black'} bg-white rounded font-bold py-2 px-4 rounded`}
 >
 {selectedCategory ? localStorage.getItem('access_token') ? "Realizar Reserva" : "Ingrese a su cuenta y seleccione una categoria" : "Ingrese a su cuenta y seleccione una categoria"}
 </button>
